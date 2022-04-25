0f74cf0d9df02b9fbde414d8af7eb457e2d971fb
Hadar Leibushor <hleibushor@gmail.com>
2022-04-25 18:37:29 +0300

    removed unecessary stuff

--- java/ValidMoves.java
+++ java/ValidMoves.java
@@ -16,24 +16,15 @@ public class ValidMoves {
 
 
     public ValidMoves(){
-        validFenceMoves = new ArrayList<Fence>();
-        validPlayerMoves = new ArrayList<PlayerMove>();
         validMoves = new ArrayList<Move>();
 
         //constants
         AMOUNT_SQUARES_ON_BOARD = 81;
         MAX_POSSIBLE_FENCE_ID = 134;
-
         
     }
 
-    public List <Fence> getValidFenceMovesLis(){
-        return this.validFenceMoves;
-    }
-
-    public List <PlayerMove> getValidPlayerMovesList(){
-        return this.validPlayerMoves;
-    }
+    
 
     public List <Move> getValidMovesList(Game game, int playerPosition){
         this.validMoves.clear();
@@ -64,61 +55,11 @@ public class ValidMoves {
 
     }
 
-    public void addPlayerMoveToValidPlayerMoveList(PlayerMove playerMove){
-        this.validPlayerMoves.add(playerMove);
-    }
-
-    public void addFenceMoveToValidFenceMoveList(Fence fence){
-        this.validFenceMoves.add(fence);
-    }
-
     public void addMoveToValidMovesList(Move move){
         this.validMoves.add(move);
         
     }
 
-    public void createValidPlayerMovesList(Game game, int playerPosition){
-
-        //clears valid move list for new turn check
-        this.validPlayerMoves.clear();
-
-        // creates valid move list
-        //iterates over possible fence IDs and checks where a fence can be placed
-        for(int i = 0; i < AMOUNT_SQUARES_ON_BOARD; i++){
-            if(game.checkIfLegalConsideringJump(playerPosition, i)){
-                PlayerMove playerMove = new PlayerMove(playerPosition, i);
-                //addPlayerMoveToValidPlayerMoveList(playerMove);
-                //Move move = new Move("m");
-                //playerMove = ((PlayerMove)move);
-                addMoveToValidMovesList(playerMove);
-
-            }
-
-        }
-
-    }
-
-    public void createValidFenceMovesList(Game game){
-
-        
-        //clears valid move list for new turn check
-        this.validFenceMoves.clear();
-
-        // creates valid move list
-        //iterates over all squares on the board and checks which are possible to jump to from cuurent position
-        for(int i = 0; i < MAX_POSSIBLE_FENCE_ID; i++){
-            getCurrABCD(i);
-            
-            Fence fence = new Fence(i, getSecondId(i), isFenceHorizontal(i), CURRENT_A, CURRENT_B, CURRENT_C, CURRENT_D);
-            if(game.checkFenceLegal(fence) == 1){
-                
-                //addFenceMoveToValidFenceMoveList(fence);
-                addMoveToValidMovesList(fence);
-            }
-
-        }
-
-    }
 
     public int getSecondId(int firstId){
         if (isFenceHorizontal(firstId))
